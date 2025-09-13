b64b72f7e458fd7f92eb63276e2c4dbee74a7650
Walter Ehrenberger <covwalter@gmail.com>
2025-09-13 13:13:49 +0200

    fixed prod client credentials

--- src/db/client.ts
+++ src/db/client.ts
@@ -5,7 +5,11 @@ import * as schema from "./schema";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL!,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
 });
 
 export const db = drizzle(pool, { schema });
