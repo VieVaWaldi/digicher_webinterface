0fcb8ea4f63d9aaa1e06f646ce6dfdd3ec47883a
Walter Ehrenberger <covwalter@gmail.com>
2025-11-10 13:53:07 +0100

    tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <script>
+        {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//kunstkosmos.de/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '3']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+      </script>
       <body className="flex h-[100dvh] flex-col">
         <Providers>
           <main className="flex-1">{children}</main>
